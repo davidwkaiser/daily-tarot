c14f7cc005ca16aab7f00c7b6652c46ef3f725ac
davidwkaiser <davidwkaiser@yahoo.com>
2017-09-01 22:09:01 -0500

    add nodemon and modify server

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app=express()
 
 app.get('/', function(req,res){
-  res.send('hello world!')
+  res.send('Welcome to the Daily Tarot Card site!')
 })
 
 app.listen(3000, function(){
