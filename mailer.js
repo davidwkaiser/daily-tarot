ecac2de61171bb19e6c6848c8be0b763eefbeae9
davidwkaiser <davidwkaiser@yahoo.com>
2018-02-04 16:53:03 -0600

    change email address

--- mailer.js
+++ mailer.js
@@ -3,8 +3,8 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
-  to: 'test@example.com',
-  from: 'test@example.com',
+  to: 'davidwkaiser@yahoo.com',
+  from: 'davidwkaiser@yahoo.com',
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
