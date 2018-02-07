22aa609ec04753333c2c185d7c96621c931b9967
davidwkaiser <davidwkaiser@yahoo.com>
2018-02-06 22:11:29 -0600

    create an ugly fix for mailer

--- app.js
+++ app.js
@@ -44,3 +44,26 @@ app.listen(process.env.PORT || 3000, function(){
 })
 
 
+//sendgrid code below
+
+var helper = require('sendgrid').mail;
+var from_email = new helper.Email('davidwkaiser@gmail.com');
+var to_email = new helper.Email('davidwkaiser@yahoo.com');
+var subject = 'Hello World from the SendGrid Node.js Library!';
+var content = new helper.Content('text/plain', 'This is a test!');
+var mail = new helper.Mail(from_email, subject, to_email, content);
+
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON(),
+});
+
+function sendMail(){
+  sg.API(request, function(error, response) {
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+  });
+}
--- mailer.js
+++ mailer.js
@@ -1,21 +1,21 @@
-var helper = require('sendgrid').mail;
-var from_email = new helper.Email('davidwkaiser@gmail.com');
-var to_email = new helper.Email('davidwkaiser@yahoo.com');
-var subject = 'Hello World from the SendGrid Node.js Library!';
-var content = new helper.Content('text/plain', 'This is a test!');
-var mail = new helper.Mail(from_email, subject, to_email, content);
+// var helper = require('sendgrid').mail;
+// var from_email = new helper.Email('davidwkaiser@gmail.com');
+// var to_email = new helper.Email('davidwkaiser@yahoo.com');
+// var subject = 'Hello World from the SendGrid Node.js Library!';
+// var content = new helper.Content('text/plain', 'This is a test!');
+// var mail = new helper.Mail(from_email, subject, to_email, content);
 
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-var request = sg.emptyRequest({
-  method: 'POST',
-  path: '/v3/mail/send',
-  body: mail.toJSON(),
-});
+// var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+// var request = sg.emptyRequest({
+//   method: 'POST',
+//   path: '/v3/mail/send',
+//   body: mail.toJSON(),
+// });
 
-function sendMail(){
-  sg.API(request, function(error, response) {
-    console.log(response.statusCode);
-    console.log(response.body);
-    console.log(response.headers);
-  });
-}
+// function sendMail(){
+//   sg.API(request, function(error, response) {
+//     console.log(response.statusCode);
+//     console.log(response.body);
+//     console.log(response.headers);
+//   });
+// }
