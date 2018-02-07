7103e9c80a9653a7f97ab69ea893f3a995fff9d8
davidwkaiser <davidwkaiser@yahoo.com>
2018-02-06 22:51:22 -0600

    remove comments

--- app.js
+++ app.js
@@ -43,29 +43,3 @@ function directionText(direction){
 app.listen(process.env.PORT || 3000, function(){
   console.log("app listening on port 3000!")
 })
-
-
-//sendgrid code below
-
-// var helper = require('sendgrid').mail;
-// var from_email = new helper.Email('davidwkaiser@gmail.com');
-// var to_email = new helper.Email('davidwkaiser@yahoo.com');
-// var subject = 'Hello World from the SendGrid Node.js Library!';
-// var content = new helper.Content('text/plain', 'This is a test!');
-// var mail = new helper.Mail(from_email, subject, to_email, content);
-
-// var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-// console.log("the API key is " + process.env.SENDGRID_API_KEY);
-// var request = sg.emptyRequest({
-//   method: 'POST',
-//   path: '/v3/mail/send',
-//   body: mail.toJSON(),
-// });
-
-// function sendMail(){
-//   sg.API(request, function(error, response) {
-//     console.log(response.statusCode);
-//     console.log(response.body);
-//     console.log(response.headers);
-//   });
-// }
