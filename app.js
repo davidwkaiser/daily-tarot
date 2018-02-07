bc533a3fb25ee6dfefc24df1ff416dedc68d67c1
davidwkaiser <davidwkaiser@yahoo.com>
2018-02-06 22:38:58 -0600

    move mailer to mailer.js

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 const data = require('./data')
-const mailer = require('./mailer')
+// const mailer = require('./mailer')
+require('./mailer')
 let cards = data.cards
 
 app.get('/', function(req, res){
@@ -46,25 +47,25 @@ app.listen(process.env.PORT || 3000, function(){
 
 //sendgrid code below
 
-var helper = require('sendgrid').mail;
-var from_email = new helper.Email('davidwkaiser@gmail.com');
-var to_email = new helper.Email('davidwkaiser@yahoo.com');
-var subject = 'Hello World from the SendGrid Node.js Library!';
-var content = new helper.Content('text/plain', 'This is a test!');
-var mail = new helper.Mail(from_email, subject, to_email, content);
+// var helper = require('sendgrid').mail;
+// var from_email = new helper.Email('davidwkaiser@gmail.com');
+// var to_email = new helper.Email('davidwkaiser@yahoo.com');
+// var subject = 'Hello World from the SendGrid Node.js Library!';
+// var content = new helper.Content('text/plain', 'This is a test!');
+// var mail = new helper.Mail(from_email, subject, to_email, content);
 
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-console.log("the API key is " + process.env.SENDGRID_API_KEY);
-var request = sg.emptyRequest({
-  method: 'POST',
-  path: '/v3/mail/send',
-  body: mail.toJSON(),
-});
+// var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+// console.log("the API key is " + process.env.SENDGRID_API_KEY);
+// var request = sg.emptyRequest({
+//   method: 'POST',
+//   path: '/v3/mail/send',
+//   body: mail.toJSON(),
+// });
 
-function sendMail(){
-  sg.API(request, function(error, response) {
-    console.log(response.statusCode);
-    console.log(response.body);
-    console.log(response.headers);
-  });
-}
+// function sendMail(){
+//   sg.API(request, function(error, response) {
+//     console.log(response.statusCode);
+//     console.log(response.body);
+//     console.log(response.headers);
+//   });
+// }
