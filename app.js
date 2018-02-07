9d0929b1eee2c076ac1fc2c5a86ac9ea45bceac9
davidwkaiser <davidwkaiser@yahoo.com>
2018-02-06 22:18:40 -0600

    modify api key reference

--- app.js
+++ app.js
@@ -53,7 +53,8 @@ var subject = 'Hello World from the SendGrid Node.js Library!';
 var content = new helper.Content('text/plain', 'This is a test!');
 var mail = new helper.Mail(from_email, subject, to_email, content);
 
-var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var sg = require('sendgrid')(process.env.SENDGRID_api_key);
+console.log("the API key is " + process.env.SENDGRID_api_key);
 var request = sg.emptyRequest({
   method: 'POST',
   path: '/v3/mail/send',
