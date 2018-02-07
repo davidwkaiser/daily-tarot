25c2422f5bd37e845eafcda579adae0bbf71edc3
davidwkaiser <davidwkaiser@yahoo.com>
2018-02-06 21:59:11 -0600

    fix mailer

--- mailer.js
+++ mailer.js
@@ -1,15 +1,21 @@
-// using SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'davidwkaiser@yahoo.com',
-  from: 'davidwkaiser@yahoo.com',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
+var helper = require('sendgrid').mail;
+var from_email = new helper.Email('davidwkaiser@gmail.com');
+var to_email = new helper.Email('davidwkaiser@yahoo.com');
+var subject = 'Hello World from the SendGrid Node.js Library!';
+var content = new helper.Content('text/plain', 'This is a test!');
+var mail = new helper.Mail(from_email, subject, to_email, content);
+
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON(),
+});
 
 function sendMail(){
-  sgMail.send(msg);
+  sg.API(request, function(error, response) {
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+  });
 }
