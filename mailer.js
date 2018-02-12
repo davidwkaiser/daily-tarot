e2ef8d3dce6eb8c53ac6ac7a02acd593e4b55926
davidwkaiser <davidwkaiser@yahoo.com>
2018-02-11 21:31:03 -0600

    refactor mailer

--- mailer.js
+++ mailer.js
@@ -2,25 +2,17 @@
 //that creates these variables and passes
 //them to the request inside sendMail
 
-var helper = require('sendgrid').mail;
-var from_email = new helper.Email('davidwkaiser@gmail.com');
-var to_email = new helper.Email('davidwkaiser@yahoo.com');
-var subject = 'Your Daily Tarot Card!';
-var content = new helper.Content('text/plain', 'This is a test!');
-var mail = new helper.Mail(from_email, subject, to_email, content);
+// var helper = require('sendgrid').mail;
+// var from_email = new helper.Email('davidwkaiser@gmail.com');
+// var to_email = new helper.Email('davidwkaiser@yahoo.com');
+// var subject = 'Your Daily Tarot Card!';
+// var content = new helper.Content('text/plain', 'This is a test!');
+// var mail = new helper.Mail(from_email, subject, to_email, content);
 
 var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
-var request = sg.emptyRequest({
-  method: 'POST',
-  path: '/v3/mail/send',
-  body: mail.toJSON(),
-});
 
 exports.sendMail = function (output){
-  content = new helper.Content('text/plain',
-    'Your card is '
-    + output.card
-    + output.txt + "!")
+  setRequest(output);
   sg.API(request, function(error, response) {
     console.log(response.statusCode);
     console.log(response.body);
@@ -28,4 +20,21 @@ exports.sendMail = function (output){
   });
 }
 
+function setRequest(output){
+  var helper = require('sendgrid').mail;
+  var from_email = new helper.Email('davidwkaiser@gmail.com');
+  var to_email = new helper.Email('davidwkaiser@yahoo.com');
+  var subject = 'Your Daily Tarot Card!';
+  var content = new helper.Content('text/plain',
+    'Your card is '
+    + output.card
+    + output.txt + "!")
+  var mail = new helper.Mail(from_email, subject, to_email, content);
+
+  var request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON(),
+  });
+}
 
