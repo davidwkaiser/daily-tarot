a4516e738f552e6afc83cea528d594c03b2f8c47
davidwkaiser <davidwkaiser@yahoo.com>
2020-04-11 14:08:53 -0500

    add explicit js anonymous functions

--- spec/AppSpec.js
+++ spec/AppSpec.js
@@ -1,58 +1,55 @@
 var Request = require("request");
 
-describe("Server", ()=> {
+describe("Server", function() {
   var server;
   beforeAll(()=> {
     server = require("../app");
     mailer = require("../mailer")
     spyOn(mailer, 'sendMail')//.and.callThrough();
   });
-  afterAll(()=> {
+  afterAll(function() {
     // server.close();
   });
 
-  describe("Get /", ()=> {
+  describe("Get /", function() {
     var data = {};
-    beforeAll((done) =>{
-      Request.get("http://localhost:3000/", (error, response, body) =>{
+    beforeAll(function(done){
+      Request.get("http://localhost:3000/", function(error, response, body){
         data.status = response.statusCode;
         data.body = body;
         done();
       });
     });
-    it("returns HTTP Status 200", ()=> {
+    it("returns HTTP Status 200", function() {
       expect(data.status).toBe(200);
     });
-    it("returns a correct Body", ()=> {
+    it("returns a correct Body", function() {
       expect(data.body).toContain("Your card is");
       expect(data.body).toMatch(/Daily Tarot Card/);
       expect(data.body).not.toMatch(/blergh/);
     });
-    it("tracks that the mailer was called", ()=> {
+    it("tracks that the mailer was called", function() {
       expect(mailer.sendMail).toHaveBeenCalled()
     });
   });
 
-  describe("POST /mailme", ()=>{
+  describe("POST /mailme", function(){
     var status = null;
     var body = {
       email:"abc@example.com",
       value: 15
   };
-    beforeAll((done)=>{
+    beforeAll(function(done){
       Request.post("http://localhost:3000/mailme", function(error, response, body){
           status = response.statusCode;
           done();
       })
     });
-    it("returns HTTP status 200", ()=>{
+    it("returns HTTP status 200", function(){
       expect(status).toBe(200);
     })
-    it("tracks that the mailer was called", ()=> {
+    it("tracks that the mailer was called", function() {
       expect(mailer.sendMail).toHaveBeenCalled()
     });
   })
-
-
-
 })
