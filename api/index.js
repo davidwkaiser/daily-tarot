e22835dbe4198fc72b01f96cc7c1cebee1d1dcd7
davidwkaiser <davidwkaiser@yahoo.com>
2025-01-31 15:01:51 -0600

    fix view directory

--- api/index.js
+++ api/index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
 app.set('view engine', 'ejs')
+app.set("views", __dirname + "/views");
 
 app.post('/mailme', function(req, res){
   let index = req.body.index
